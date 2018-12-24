19c19143eaa98900379e19ccb888c3409858e70f
Maulik Sharma <mauliksharma@Mauliks-MacBook.local>
2018-12-24 08:57:36 +0530

    Implemented mining

--- Sources/App/BlockchainService.swift
+++ Sources/App/BlockchainService.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class BlockchainService {
+    
+    var blockchain :Blockchain!
+    
+    init() {
+        self.blockchain = Blockchain(genesisBlock: Block())
+    }
+    
+    func getNextBlock(transactions :[Transaction]) -> Block {
+        let block = self.blockchain.getNextBlock(transactions: transactions)
+        self.blockchain.addBlock(block)
+        return block
+    }
+    
+    func getBlockchain() -> Blockchain {
+        return self.blockchain
+    }
+    
+}
--- Sources/App/Controllers/BlockchainController.swift
+++ Sources/App/Controllers/BlockchainController.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Vapor
+
+class BlockchainController {
+    
+    var blockchainService :BlockchainService
+    
+    init() {
+        self.blockchainService = BlockchainService()
+    }
+    
+    func mine(req :Request, transaction :Transaction) -> Block {
+        return self.blockchainService.getNextBlock(transactions :[transaction])
+    }
+    
+    func getBlockchain(req :Request) -> Blockchain {
+        return self.blockchainService.getBlockchain()
+    }
+    
+}
